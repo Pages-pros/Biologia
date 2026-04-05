33022f76174136102567c6c16a0246c38bd2ce70
Gabriel <109928103+Pages-pros@users.noreply.github.com>
2026-04-05 12:02:28 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Initialize AOS (Animate on Scroll)
-    AOS.init({
-        once: true, // whether animation should happen only once - while scrolling down
-        offset: 100, // offset (in px) from the original trigger point
-        duration: 800, // values from 0 to 3000, with step 50ms
-        easing: 'ease-out-cubic', // default easing for AOS animations
-    });
-});
